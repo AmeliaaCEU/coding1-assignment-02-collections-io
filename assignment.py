a67aa9f11509dd9beed489a9e084c84dbc91c4f9
Amelia Arthur <arthur_amelia@student.ceu.edu>
2026-09-23 17:18:00 +0200

    Add explanatory comments to assignment script

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" #defines the variable drink as "tea"
+sales = {"tea": 3, "coffee": 5} #defines a dictionary called sales with keys "tea" and "coffee" values for each
+sold = sales[drink] #uses sales dictionary to find the value of the key "tea" and assigns it to the variable sold
+sold = sold + 1 #adds 1 to the value of sold
+print(drink) #prints the value of the variable drink
+print(sold) #prints the number of drinks sold after adding above.
